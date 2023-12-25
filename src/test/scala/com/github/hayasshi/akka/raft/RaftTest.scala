0af2ad8e09bcbedfd416320293237b2960c01459
hayasshi <daisuke0884@gmail.com>
2023-12-25 20:42:51 +0900

    Modify test

--- src/test/scala/com/github/hayasshi/akka/raft/RaftTest.scala
+++ src/test/scala/com/github/hayasshi/akka/raft/RaftTest.scala
@@ -37,21 +37,46 @@ class RaftTest extends TestKit(ActorSystem("RaftTest")) with ImplicitSender with
           system.log.info("No leader yet.")
       }
     }
+
+    def stopLeader(initial: ActorRef): Unit = {
+      (initial ? RaftProtocol.Command(0)).foreach {
+        case _: RaftProtocol.Accepted =>
+          system.log.info(s"Stop leader: $initial")
+          system.stop(initial)
+        case Follower.RedirectTo(Some(leader), _) =>
+          system.log.info(s"Stop leader: $leader")
+          system.stop(leader)
+        case Follower.RedirectTo(None, _) =>
+          system.log.info("No leader yet.")
+      }
+    }
       
     "Test1" in {
       val member1 = system.actorOf(Props[Target](new Target), "member1")
       val member2 = system.actorOf(Props[Target](new Target), "member2")
       val member3 = system.actorOf(Props[Target](new Target), "member3")
+      val member4 = system.actorOf(Props[Target](new Target), "member4")
+      val member5 = system.actorOf(Props[Target](new Target), "member5")
 
-      member1 ! RaftNode.Start(Set(member2, member3))
-      member2 ! RaftNode.Start(Set(member1, member3))
-      member3 ! RaftNode.Start(Set(member1, member2))
+      member1 ! RaftNode.Start(Set(member2, member3, member4, member5))
+      member2 ! RaftNode.Start(Set(member1, member3, member4, member5))
+      member3 ! RaftNode.Start(Set(member1, member2, member4, member5))
+      member4 ! RaftNode.Start(Set(member1, member2, member3, member5))
+      member5 ! RaftNode.Start(Set(member1, member2, member3, member4))
 
       Thread.sleep(10 * 1000)
 
       askMessage(member3, RaftProtocol.Command(2))
 
+      Thread.sleep(2 * 1000)
+
+      stopLeader(member1)
+
       Thread.sleep(5 * 1000)
+
+      askMessage(member2, RaftProtocol.Command(3))
+
+      Thread.sleep(3 * 1000)
 
       Await.result(system.terminate(), 10.seconds)
     }
